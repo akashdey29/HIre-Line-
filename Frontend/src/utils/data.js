778f5907904ec0693de4d535feac029430cb4fc3
akashdey29 <akashdey2901@gmail.com>
2025-10-16 13:25:00 +0530

    First Commit

---
+++ Frontend/src/utils/data.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://hireline-j3s9.onrender.com/api";
+
+export const USER_API_ENDPOINT = `${BASE_URL}/user`;
+export const JOB_API_ENDPOINT = `${BASE_URL}/job`;
+export const APPLICATION_API_ENDPOINT = `${BASE_URL}/application`;
+export const COMPANY_API_ENDPOINT = `${BASE_URL}/company`;
